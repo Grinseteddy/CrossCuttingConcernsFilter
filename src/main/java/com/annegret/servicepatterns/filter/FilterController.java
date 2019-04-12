86e36d04c488cc87930dd39e1f77e38ef0bd2d6e
Grinseteddy <annegret.kampe@arcor.de>
2019-04-12 17:56:12 +0200

    Logging as Json

--- src/main/java/com/annegret/servicepatterns/filter/FilterController.java
+++ src/main/java/com/annegret/servicepatterns/filter/FilterController.java
@@ -1,5 +1,7 @@
 package com.annegret.servicepatterns.filter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 public class FilterController {
+
+    static Logger logger=LoggerFactory.getLogger(FilterController.class);
+
     @GetMapping(value ="filter")
     public String filtered(@RequestParam("inputString") String inputString) {
         String returnString = filter(inputString);
+        logger.info(inputString+" --> "+returnString);
         return returnString;
     }
 
