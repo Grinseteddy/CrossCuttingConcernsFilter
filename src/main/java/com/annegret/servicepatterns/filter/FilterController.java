365c6ee29f04159142a6f2eafce3a466b528a9c5
Grinseteddy <annegret.kampe@arcor.de>
2019-04-14 17:01:56 +0200

    Index not read yet

--- src/main/java/com/annegret/servicepatterns/filter/FilterController.java
+++ src/main/java/com/annegret/servicepatterns/filter/FilterController.java
@@ -2,20 +2,27 @@ package com.annegret.servicepatterns.filter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
+@RefreshScope
 @RestController
-
+@ConfigurationProperties
 public class FilterController {
 
     static Logger logger=LoggerFactory.getLogger(FilterController.class);
 
+    @Value("${index:1}")
+    private String index;
+
     @GetMapping(value ="filter")
     public String filtered(@RequestParam("inputString") String inputString) {
-        String returnString = filter(inputString);
+        String returnString = filter(inputString)+" "+index;
         logger.info(inputString+" --> "+returnString);
         return returnString;
     }
